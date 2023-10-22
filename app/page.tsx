7fd6d49c4ada718b6d7470c67084adbdcd46b532
Ishan Pradhan <ipradhan@cpaxtra.co.th>
2023-10-22 13:30:36 +0700

    add sprites and project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
+import x from "@/public/images/sprites/aggron.png";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Image src={x} alt="s" />
+    </div>
+  );
 }
