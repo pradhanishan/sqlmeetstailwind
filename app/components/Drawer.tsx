4e4d676f25379017fc19b95fb0cc79f9a9e8d239
Ishan Pradhan <ipradhan@cpaxtra.co.th>
2023-10-23 08:22:09 +0700

    add backdrop to navigation drawer

--- app/components/Drawer.tsx
+++ app/components/Drawer.tsx
@@ -4,13 +4,22 @@ import DrawerCloseButton from "./DrawerCloseButton";
 
 export default function Drawer({ closeDrawer }: { closeDrawer: () => void }) {
   return (
-    <div
-      className="h-screen fixed left-0 top-0 
-        w-[250px]
-        bg-gradient-to-br from-gray-400 to-slate-300
+    <div className="h-screen fixed left-0 top-0 flex w-screen">
+      {/* drawer */}
+      <div
+        className="
+        w-[250px] h-full
+        bg-gray-400
+       
         dark:bg-gradient-to-br dark:from-slate-900 dark:to-sky-900"
-    >
-      <DrawerCloseButton closeDrawer={closeDrawer} />
+      >
+        <DrawerCloseButton closeDrawer={closeDrawer} />
+      </div>
+      {/* backdron */}
+      <div
+        className="flex-grow backdrop-blur-sm border-l border-slate-500 dark:border-sky-800 "
+        onClick={closeDrawer}
+      ></div>
     </div>
   );
 }
--- app/components/DrawerCloseButton.tsx
+++ app/components/DrawerCloseButton.tsx
@@ -18,7 +18,7 @@ export default function DrawerCloseButton({
       strokeLinejoin="round"
       onClick={() => {
         closeDrawer();
-      }}
+      }} className="sm:hidden"
     >
       <line x1="18" y1="6" x2="6" y2="18"></line>
       <line x1="6" y1="6" x2="18" y2="18"></line>
